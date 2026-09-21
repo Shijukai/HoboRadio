7eb8f33878f9f9dffe32a6a70391c7280de33b34
s0b4yu <haduki.kikyou.168@gmail.com>
2026-09-22 08:30:20 +0900

    feat(tape): HoboTapeスクリプトおよび関連アセットを追加

---
+++ Packages/com.shijukai.hoboradio/Runtime/Script/HoboTape.cs
@@ -0,0 +1,24 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+[UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+public class HoboTape : UdonSharpBehaviour
+{
+    [Header("Tape Info")]
+    public VRCUrl tapeUrl;
+    public string tapeTitle;
+    [Tooltip("制作団体・サークル名")]
+    public string tapeArtist;
+
+    [Header("Components")]
+    public VRC_Pickup pickup;
+    public Collider tapeCollider;
+
+    private void Start()
+    {
+        if (pickup == null) pickup = (VRC_Pickup)GetComponent(typeof(VRC_Pickup));
+        if (tapeCollider == null) tapeCollider = GetComponent<Collider>();
+    }
+}
