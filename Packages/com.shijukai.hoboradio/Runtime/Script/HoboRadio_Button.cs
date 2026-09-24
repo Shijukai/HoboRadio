73fa5698a152c6b96b14a9949e1d9094b742d6ba
s0b4yu <haduki.kikyou.168@gmail.com>
2026-09-25 07:10:12 +0900

    feat(button): 各種ボタン処理をHoboRadio_Buttonスクリプトに統合

---
+++ Packages/com.shijukai.hoboradio/Runtime/Script/HoboRadio_Button.cs
@@ -0,0 +1,58 @@
+﻿using UdonSharp;
+using UnityEngine;
+
+public enum RadioButtonType
+{
+    Power,
+    SwitchChannel,
+    Play,
+    Pause,
+    Stop,
+    FastForward,
+    Rewind,
+    Debug,
+    Eject
+}
+
+[UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+public class HoboRadio_Button : UdonSharpBehaviour
+{
+    [SerializeField]
+    private UdonSharpBehaviour targetUdon;
+
+    [SerializeField]
+    private RadioButtonType buttonType;
+
+    public override void Interact()
+    {
+        if (targetUdon == null) return;
+
+        switch (buttonType)
+        {
+            case RadioButtonType.Power:
+                targetUdon.SendCustomEvent("InteractButtonPower");
+                break;
+            case RadioButtonType.SwitchChannel:
+                targetUdon.SendCustomEvent("InteractSwitchChannel");
+                break;
+            case RadioButtonType.Play:
+                targetUdon.SendCustomEvent("InteractButtonPlay");
+                break;
+            case RadioButtonType.Pause:
+                targetUdon.SendCustomEvent("InteractButtonPause");
+                break;
+            case RadioButtonType.Stop:
+                targetUdon.SendCustomEvent("InteractButtonStop");
+                break;
+            case RadioButtonType.FastForward:
+                targetUdon.SendCustomEvent("InteractButtonFastForward");
+                break;
+            case RadioButtonType.Rewind:
+                targetUdon.SendCustomEvent("InteractButtonRewind");
+                break;
+            case RadioButtonType.Debug:
+                targetUdon.SendCustomEvent("InteractButtonDebug");
+                break;
+        }
+    }
+}
